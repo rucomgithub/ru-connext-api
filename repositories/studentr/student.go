fb7815214e57299496e90aa7c8c324c86c892a18
nahmkahw <nahmkahw@gmail.com>
2023-07-01 12:55:19 +0700

    add route student

--- repositories/studentr/student.go
+++ repositories/studentr/student.go
@@ -3,7 +3,8 @@ package studentr
 func (r *studentRepoDB) GetStudentAll() (*[]StudentRepo, error) {
 
 	students := []StudentRepo{}
-	query := "SELECT STD_CODE,CREDIT FROM DBBACH00.UGB_REGIS_RU24 WHERE STD_CODE like '65%' and YEAR = 2565 ORDER BY YEAR DESC, SEMESTER DESC"
+
+	query := "SELECT STD_CODE FROM DBBACH00.UGB_REGIS_RU24 WHERE STD_CODE like '65%' and YEAR = 2565 ORDER BY YEAR DESC, SEMESTER DESC"
 
 	err := r.oracle_db.Select(&students, query)
 	if err != nil {
