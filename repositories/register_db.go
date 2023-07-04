0e8a89be26b0dc4958500e86108fdcc36e535c2c
nahmkahw <nahmkahw@gmail.com>
2023-07-04 14:58:29 +0700

    register schedulelatest order year and semester

--- repositories/register_db.go
+++ repositories/register_db.go
@@ -103,7 +103,7 @@ where semester=:param11 and year=:param12 and  std_code= :param13
 func (r *registerRepoDB) GetYearSemesterLatest() (student *YearSemesterRepo, err error) {
 
 	yearsemester := YearSemesterRepo{}
-	query := `SELECT CURRENT_YEAR YEAR, CURRENT_SEMESTER SEMESTER FROM CURRENT_REGISTER WHERE ROWNUM = 1`
+	query := `SELECT CURRENT_YEAR YEAR, CURRENT_SEMESTER SEMESTER FROM CURRENT_REGISTER WHERE ROWNUM = 1 ORDER BY CURRENT_YEAR, CURRENT_SEMESTER`
 
 	err = r.oracle_db.Get(&yearsemester, query)
 	if err != nil {
