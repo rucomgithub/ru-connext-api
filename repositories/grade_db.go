a39140b65994a3fe2123bb6a55cce79150b64d62
Adithep Kitikadaeng <adithep@ru.ac.th>
2023-07-18 14:15:21 +0700

    Update grade_db.go

--- repositories/grade_db.go
+++ repositories/grade_db.go
@@ -5,7 +5,10 @@ import "fmt"
 func (r *gradeRepoDB) GetGradeYear(std_code, year string) (*[]GradeRepo, error) {
 	if std_code == "6299999991" {
 		std_code = "6407501375"
-		//std_code = "6006416793"
+		fmt.Printf("register: %s \n", std_code)
+	}
+	if std_code == "6299999992" {
+		std_code = "6390400031"
 		fmt.Printf("register: %s \n", std_code)
 	}
 	grade := []GradeRepo{}
@@ -25,7 +28,10 @@ func (r *gradeRepoDB) GetGradeYear(std_code, year string) (*[]GradeRepo, error) 
 func (r *gradeRepoDB) GetGradeAll(std_code string) (*[]GradeRepo, error) {
 	if std_code == "6299999991" {
 		std_code = "6407501375"
-		//std_code = "6006416793"
+		fmt.Printf("register: %s \n", std_code)
+	}
+	if std_code == "6299999992" {
+		std_code = "6390400031"
 		fmt.Printf("register: %s \n", std_code)
 	}
 	grade := []GradeRepo{}
