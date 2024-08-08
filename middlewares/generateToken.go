df82367f9d667a03ab08e393136b1c76e57ca768
rucomgithub <101234926+rucomgithub@users.noreply.github.com>
2024-08-08 12:11:13 +0700

    Update generateToken.go set expire token 1 minute

--- middlewares/generateToken.go
+++ middlewares/generateToken.go
@@ -17,7 +17,7 @@ func GenerateToken(stdCode,role string, redis_cache *redis.Client) (*TokenRespon
 	//expirationAccessToken := time.Now().AddDate(0, 0, 1).Unix()
 	expirationAccessToken := time.Now().Add(time.Minute * 1).Unix()
 	//expirationRefreshToken := time.Now().AddDate(0, 1, 0).Unix()
-	expirationRefreshToken := time.Now().Add(time.Minute * 6).Unix()
+	expirationRefreshToken := time.Now().Add(time.Minute * 2).Unix()
 
 	generateToken.IsAuth = true
 	generateToken.AccessTokenKey = stdCode + "::access::" + uuid.New().String()
