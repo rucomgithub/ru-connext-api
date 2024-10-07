1dea1a13dc85fa1d85852bcfcbb1d4aaf3a9ece3
nahmkahw <nahmkahw@gmail.com>
2024-10-07 10:09:45 +0700

    format date graduate date

--- repositories/masterrepo/master_repo_success.go
+++ repositories/masterrepo/master_repo_success.go
@@ -18,14 +18,14 @@ func (r *studentRepoDB) GetStudentSuccess(studentCode string) (student *StudentS
 	NVL(g.PLAN,'-') PLAN,
 	g.CONFERENCE_NO,
 	g.SERIAL_NO,
-	TO_CHAR(TO_DATE(g.conference_date,'DD/MM/YYYY'),'dd MON yyyy','NLS_CALENDAR=''THAI BUDDHA'' NLS_DATE_LANGUAGE=THAI') conference_date,
-	TO_CHAR(TO_DATE(g.graduated_date,'DD/MM/YYYY'),'dd MON yyyy','NLS_CALENDAR=''THAI BUDDHA'' NLS_DATE_LANGUAGE=THAI') graduated_date,
-	TO_CHAR(TO_DATE(g.conference_date,'DD/MM/YYYY')+7,'dd MON yyyy','NLS_CALENDAR=''THAI BUDDHA'' NLS_DATE_LANGUAGE=THAI') confirm_date, 
+	TO_CHAR(TO_DATE(g.conference_date,'DD/MM/YYYY'),'FMDD MONTH YYYY','NLS_CALENDAR=''THAI BUDDHA'' NLS_DATE_LANGUAGE=THAI') conference_date,
+	TO_CHAR(TO_DATE(g.graduated_date,'DD/MM/YYYY'),'FMDD MONTH YYYY','NLS_CALENDAR=''THAI BUDDHA'' NLS_DATE_LANGUAGE=THAI') graduated_date,
+	TO_CHAR(TO_DATE(g.conference_date,'DD/MM/YYYY')+7,'FMDD MONTH YYYY','NLS_CALENDAR=''THAI BUDDHA'' NLS_DATE_LANGUAGE=THAI') confirm_date, 
 	nvl(nvl(g.main_major_thai,g.major_name) , '-') major_name_thai 
 	from  vm_graduate g 
 	where  std_code = :param1`
 
-	fmt.Printf("success: %s \n", studentCode) 
+	fmt.Printf("success: %s \n", studentCode)
 
 	err = r.oracle_db_dbg.Get(&student_info, query, studentCode)
 
