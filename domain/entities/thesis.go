b7af03afa780c31e8fbeaae18138435319b7f943
nahmkahw <nahmkahw@gmail.com>
2025-12-19 09:30:49 +0700

    add list and get requestsuccess of officer

--- domain/entities/thesis.go
+++ domain/entities/thesis.go
@@ -81,19 +81,50 @@ type OtherPublication struct {
 	CreatedAt    time.Time `json:"createdAt" db:"CREATED_AT"`
 }
 
-// DB model — ใช้กับ sqlx / database/sql
 type RequestSuccess struct {
-	STD_CODE         string `json:"STD_CODE" validate:"min=9,max=10,regexp=^[0-9]" db:"STD_CODE"`
-	SUCCESS_YEAR     string `json:"SUCCESS_YEAR" validate:"min=4,max=4,regexp=^[0-9]" db:"SUCCESS_YEAR"`
-	SUCCESS_SEMESTER string `json:"SUCCESS_SEMESTER" validate:"min=1,max=1,regexp=^[0-9]" db:"SUCCESS_SEMESTER"`
-	NAME_THAI        string `json:"NAME_THAI" db:"NAME_THAI"`
-	NAME_ENG         string `json:"NAME_ENG" db:"NAME_ENG"`
-	DEGREE           string `json:"DEGREE" db:"DEGREE"`
-	THESIS_THAI      string `json:"THESIS_THAI" db:"THESIS_THAI"`
-	THESIS_ENG       string `json:"THESIS_ENG" db:"THESIS_ENG"`
-	REGISTRATION     string `json:"REGISTRATION" db:"REGISTRATION"`
-	GRADES           string `json:"GRADES" db:"GRADES"`
-	ADDRESS          string `json:"ADDRESS" db:"ADDRESS"`
-	CREATED          string `json:"CREATED" db:"CREATED"`
-	MODIFIED         string `json:"MODIFIED" db:"MODIFIED"`
+	ENROLL_YEAR          string `db:"ENROLL_YEAR" json:"enrollYear"`
+	ENROLL_SEMESTER      string `db:"ENROLL_SEMESTER" json:"enrollSemester"`
+	STD_CODE             string `db:"STD_CODE" json:"stdCode"`
+	PRENAME_THAI_S       string `db:"PRENAME_THAI_S" json:"prenameThai"`
+	PRENAME_ENG_S        string `db:"PRENAME_ENG_S" json:"prenameEng"`
+	FIRST_NAME           string `db:"FIRST_NAME" json:"firstName"`
+	LAST_NAME            string `db:"LAST_NAME" json:"lastName"`
+	FIRST_NAME_ENG       string `db:"FIRST_NAME_ENG" json:"firstNameEng"`
+	LAST_NAME_ENG        string `db:"LAST_NAME_ENG" json:"lastNameEng"`
+	THAI_NAME            string `db:"THAI_NAME" json:"thaiName"`
+	PLAN_NO              string `db:"PLAN_NO" json:"planNo"` 
+	SEX                  string `db:"SEX" json:"sex"`
+	REGINAL_NAME         string `db:"REGINAL_NAME" json:"reginalName"`
+	SUBSIDY_NAME         string `db:"SUBSIDY_NAME" json:"subsidyName"`
+	STATUS_NAME_THAI     string `db:"STATUS_NAME_THAI" json:"statusNameThai"`
+	BIRTH_DATE           string `db:"BIRTH_DATE" json:"birthDate"`
+	STD_ADDR             string `db:"STD_ADDR" json:"studentAddress"`
+	ADDR_TEL             string `db:"ADDR_TEL" json:"addressTel"`
+	JOB_POSITION         string `db:"JOB_POSITION" json:"jobPosition"`
+	STD_OFFICE           string `db:"STD_OFFICE" json:"studentOffice"`
+	OFFICE_TEL           string `db:"OFFICE_TEL" json:"officeTel"`
+	DEGREE_NAME          string `db:"DEGREE_NAME" json:"degreeName"`
+	BSC_DEGREE_NO        string `db:"BSC_DEGREE_NO" json:"bscDegreeNo"`
+	BSC_DEGREE_THAI_NAME string `db:"BSC_DEGREE_THAI_NAME" json:"bscDegreeThaiName"`
+	BSC_INSTITUTE_NO     string `db:"BSC_INSTITUTE_NO" json:"bscInstituteNo"`
+	INSTITUTE_THAI_NAME  string `db:"INSTITUTE_THAI_NAME" json:"instituteThaiName"`
+	CK_CERT_NO           string `db:"CK_CERT_NO" json:"ckCertNo"`
+	CHK_CERT_NAME_THAI   string `db:"CHK_CERT_NAME_THAI" json:"chkCertNameThai"`
+	SUCCESS_YEAR         string `db:"SUCCESS_YEAR" json:"successYear"`
+	SUCCESS_SEMESTER     string `db:"SUCCESS_SEMESTER" json:"successSemester"`
+	NAME_THAI            string `db:"NAME_THAI" json:"nameThai"`
+	NAME_ENG             string `db:"NAME_ENG" json:"nameEng"`
+	THESIS_THAI          string `db:"THESIS_THAI" json:"thesisThai"`
+	THESIS_ENG           string `db:"THESIS_ENG" json:"thesisEng"`
+	DEGREE               string `db:"DEGREE" json:"degree"`
+	REGISTRATION         string `db:"REGISTRATION" json:"registration"`
+	GRADES               string `db:"GRADES" json:"grades"`
+	ADDRESS              string `db:"ADDRESS" json:"address"`
+	CREATED              string `db:"CREATED" json:"created"`
+	MODIFIED             string `db:"MODIFIED" json:"modified"`
+	THESIS_THAI_TITLE    string `db:"THESIS_THAI_TITLE" json:"thesisThaiTitle"`
+	THESIS_ENG_TITLE     string `db:"THESIS_ENG_TITLE" json:"thesisEngTitle"`
+	THESIS_TYPE          string `db:"THESIS_TYPE" json:"thesisType"`
+	SIMILARITY           string `db:"SIMILARITY" json:"similarity"`
 }
+
--- infrastructure/db/requestsuccess_repository.go
+++ infrastructure/db/requestsuccess_repository.go
@@ -44,23 +44,55 @@ func (r *thesisJournalRepository) GetRequestSuccessByID(ctx context.Context, id 
 func (r *thesisJournalRepository) ListRequestSuccess(ctx context.Context, limit, offset int) ([]*entities.RequestSuccess, error) {
 	requestSuccess := []*entities.RequestSuccess{}
 
-	query := `
-		SELECT
-			STD_CODE,
-			SUCCESS_YEAR,
-			SUCCESS_SEMESTER,
-			NAME_THAI,
-			NAME_ENG ,
-			THESIS_THAI,
-			THESIS_ENG,
-			DEGREE,
-			REGISTRATION,
-			GRADES,
-			ADDRESS,
-			CREATED,
-			MODIFIED
-			FROM EGRAD_REQUEST_SUCCESS
-        ORDER BY CREATED DESC
+	query := `SELECT  
+			VSS.ENROLL_YEAR,
+			VSS.ENROLL_SEMESTER,
+			VSS.STD_CODE,
+			VSS.PRENAME_THAI_S,
+			VSS.PRENAME_ENG_S,
+			VSS.FIRST_NAME,
+			VSS.LAST_NAME,
+			VSS.FIRST_NAME_ENG,
+			VSS.LAST_NAME_ENG,
+			VSS.THAI_NAME,
+			VSS.PLAN_NO,
+			VSS.SEX,
+			VSS.REGINAL_NAME,
+			VSS.SUBSIDY_NAME,
+			VSS.STATUS_NAME_THAI,
+			VSS.BIRTH_DATE,
+			VSS.STD_ADDR,
+			VSS.ADDR_TEL,
+			VSS.JOB_POSITION,
+			VSS.STD_OFFICE,
+			VSS.OFFICE_TEL,
+			VSS.DEGREE_NAME,
+			VSS.BSC_DEGREE_NO,
+			VSS.BSC_DEGREE_THAI_NAME,
+			VSS.BSC_INSTITUTE_NO,
+			VSS.INSTITUTE_THAI_NAME,
+			VSS.CK_CERT_NO,
+			VSS.CHK_CERT_NAME_THAI,
+			NVL(ES.SUCCESS_YEAR, '-') AS SUCCESS_YEAR,
+			NVL(ES.SUCCESS_SEMESTER, '-') AS SUCCESS_SEMESTER,
+			NVL(ES.NAME_THAI, '-') AS NAME_THAI,
+			NVL(ES.NAME_ENG, '-') AS NAME_ENG,
+			NVL(ES.DEGREE, '-') AS DEGREE,
+			NVL(ES.THESIS_THAI, '-') AS THESIS_THAI,
+			NVL(ES.THESIS_ENG, '-') AS THESIS_ENG,
+			NVL(ES.REGISTRATION, '-') AS REGISTRATION,
+			NVL(ES.GRADES, '-') AS GRADES,
+			NVL(ES.ADDRESS, '-') AS ADDRESS,
+			NVL(TO_CHAR(ES.CREATED, 'YYYY-MM-DD HH24:MI:SS'), '-') AS CREATED,
+			NVL(TO_CHAR(ES.MODIFIED, 'YYYY-MM-DD HH24:MI:SS'), '-') AS MODIFIED,
+			NVL(DECODE(T.THESIS_TITLE_THAI, NULL, '-', T.THESIS_TITLE_THAI), '-') AS THESIS_THAI_TITLE,
+			NVL(DECODE(T.THESIS_TITLE_ENGLISH, NULL, '-', T.THESIS_TITLE_ENGLISH), '-') AS THESIS_ENG_TITLE,
+			NVL(T.THESIS_TYPE, '-') AS THESIS_TYPE,
+			NVL(T.SIMILARITY, -1) AS SIMILARITY 
+			from egrad_request_success es
+			inner join VM_STUDENT_S VSS on vss.std_code = es.std_code
+			inner join egrad_thesis_similarity t on vss.std_code = t.std_code
+        ORDER BY es.CREATED DESC
         OFFSET :1 ROWS FETCH NEXT :2 ROWS ONLY`
 
 	err := r.db.SelectContext(ctx, &requestSuccess, query, offset, limit)
